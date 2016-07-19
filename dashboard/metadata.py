bd6325fb2f8aa97a940f99cde3ee06e6dc439632
Baofeng Dong <dongb@trimet.org>
2016-07-19 09:35:22 -0700

    created div and table headers lookup metadata for survey questions

---
+++ dashboard/metadata.py
@@ -0,0 +1,50 @@
+metadata = {
+    1: {
+        "headers": ["Transfer", "Count", "Pct"],
+        "id": "transfer"
+        },
+    2: {
+        "headers": ["Range", "Count", "Pct"],
+        "id": "trip"
+        },
+    3: {
+        "headers": ["Fare Agency", "Count", "Pct"],
+        "id": "agency"
+        },
+    4: {
+        "headers": ["Fare Type", "Count", "Pct"],
+        "id": "faretype"
+        },
+    5: {
+        "headers": ["Purchase Type", "Count", "Pct"],
+        "id": "purchasetype"
+        },        
+    6: {
+        "headers": ["Trip", "Count", "Pct"],
+        "id": "daypass"
+        },
+    7: {
+        "headers": ["Trip Type", "Count", "Pct"],
+        "id": "singlefare"
+        }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
